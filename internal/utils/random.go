b575c721c43191836e8ce95de4589154c792f27b
0xBECEDA <sakharovasonya1@gmail.com>
2023-11-07 14:16:17 +0100

    comment

--- internal/utils/random.go
+++ internal/utils/random.go
@@ -7,6 +7,10 @@ import (
 
 const maxNum = 100000
 
+// GenerateRandomNonZeroValue generates random non-zero value, which is not bigger than maxNum
+// We limit the random number since it will be used for exponentiation with a big int.
+// As we are dealing with numbers greater than int64, unrestricted exponentiation would require
+// significant computational resources
 func GenerateRandomNonZeroValue() *big.Int {
 	num := rand.Intn(maxNum)
 	if num == 0 {
